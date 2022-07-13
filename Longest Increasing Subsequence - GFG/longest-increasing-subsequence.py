30ca059e196fb011961a8d655aecfb7057560c4a
srikar akkina <51265375+akkinasrikar@users.noreply.github.com>
2022-07-13 17:35:19 +0530

    Added solution - LeetHub

---
+++ Longest Increasing Subsequence - GFG/longest-increasing-subsequence.py
@@ -0,0 +1,27 @@
+#User function Template for python3
+
+class Solution:
+    
+    #Function to find length of longest increasing subsequence.
+    def longestSubsequence(self,nums,n):
+        # code here
+        dp=[1]*n
+        for i in range(n-1,-1,-1):
+            for j in range(i+1,n):
+                if nums[i]<nums[j]:
+                    dp[i]=max(dp[i],1+dp[j])
+        return max(dp)
+       
+
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    for _ in range(int(input())):
+        n = int(input())
+        a = [ int(x) for x in input().split() ]
+        ob=Solution()
+        print(ob.longestSubsequence(a,n))
+# } Driver Code Ends
