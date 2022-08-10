c2f49284d590faaeaeea09157858c8b0d3532b10
srikar akkina <51265375+akkinasrikar@users.noreply.github.com>
2022-08-10 16:04:13 +0530

    Time: 42 ms (75.00%), Space: 14 MB (8.33%) - LeetHub

--- 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.py
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.py
@@ -1,23 +1,8 @@
 class Solution:
     def arithmeticTriplets(self, nums: List[int], diff: int) -> int:
+        hs=set(nums)
         res=0
-        l,m=0,1
-        while l<len(nums)-2:
-            while m<len(nums)-1:
-                diff1=nums[m]-nums[l]
-                if diff1==diff:
-                    r=m+1
-                    while r<len(nums):
-                        diff2=nums[r]-nums[m]
-                        if diff2==diff1:
-                            #print(l,m,r,nums[l],nums[m],nums[r])
-                            res += 1
-                        if diff2<diff1: r += 1
-                        else: break
-                m += 1
-            l += 1
-            m = l+1
+        for i in nums:
+            if i-diff in hs and i+diff in hs: res += 1
         return res
-            
-            
         
