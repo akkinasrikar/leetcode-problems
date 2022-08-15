18c3a844d67a13e779bc4419b57bb5043e7a2ae7
srikar akkina <51265375+akkinasrikar@users.noreply.github.com>
2022-08-15 09:55:50 +0530

    Time: 78 ms (48.93%), Space: 13.9 MB (76.66%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def romanToInt(self, s):
+        rc={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
+        s = s.replace("IV", "IIII").replace("IX", "VIIII")
+        s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
+        s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+        val=0
+        for i in s:
+            val=val+rc[i]
+        return val
