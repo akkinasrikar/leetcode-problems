12398f10b67ba07479d0a34c8cd8c4aa1db902a3
srikar akkina <51265375+akkinasrikar@users.noreply.github.com>
2022-09-10 10:20:25 +0530

    Time: 5046 ms (5.00%), Space: 27.5 MB (34.87%) - LeetHub

---
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
@@ -0,0 +1,34 @@
+class Solution:
+    def findKthLargest(self, nums: List[int], x: int) -> int:
+        
+        def mergesort(arr):
+            if len(arr)>1: 
+                mid = len(arr)//2
+                L,R=arr[:mid],arr[mid:]
+                mergesort(L)
+                mergesort(R)
+
+                i = j = k = 0
+
+                while i<len(L) and j<len(R):
+                    if L[i]<R[j]:
+                        arr[k] = L[i]
+                        i += 1
+                    else:
+                        arr[k] = R[j]
+                        j += 1
+                    k += 1
+
+                while i<len(L):
+                    arr[k] = L[i]
+                    i += 1
+                    k += 1
+
+                while j<len(R):
+                    arr[k] = R[j]
+                    j += 1
+                    k += 1
+        
+        mergesort(nums)
+        return nums[len(nums)-x]
+        
